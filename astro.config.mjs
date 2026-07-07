bf8e8e104f03b48f741189a35a2b092be5a15d85
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2026-07-07 18:01:06 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import solidJs from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://newconstitution.pages.dev",
-  integrations: [solidJs()],
+  integrations: [solidJs(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],

ef2f1531aaab4233a73041178ca14da530bffa99
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2026-07-07 18:01:13 +0200

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
+import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
